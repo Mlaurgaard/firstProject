344fa2396f03ee93c0269cd6ed1c62ccffc8ed33
Mlaurgaard <m.laurgaard@gmail.com>
2023-09-26 14:55:37 +0200

    bunch of things

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+/* Variabel
+- Er var, let og const
+- Var = Ikke bruk
+- Let kan definere variabel og skifte value
+- Const definer variabel men verdien er satt fast
+*/
+let isLoggedIn = false;
+isLoggedIn = true;
+
+
+console.log("Am i logged in?", isLoggedIn);
