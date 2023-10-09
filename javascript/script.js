81469277cdbfa34fc14f8e7c6177367cb79dc782
Mlaurgaard <m.laurgaard@gmail.com>
2023-10-09 20:17:23 +0200

    Modal
    Added a modal window to contacts.
    -added modal to html
    - new css class for modal
    -js written for modal window

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,39 @@
 /* Variabel
 - Er var, let og const
 - Var = Ikke bruk
-- Let kan definere variabel og skifte value
+- Let kan definere variabel og skifte verdier
 - Const definer variabel men verdien er satt fast
 */
-let isLoggedIn = false;
+
+/*let isLoggedIn = false;
 isLoggedIn = true;
 
 
 console.log("Am i logged in?", isLoggedIn);
+*/
+
+function Contact() {
+    alert ("Contact page!");
+}
+
+/*Get Modal*/
+let modal = document.getElementById("myModal");
+
+/* Modal Button */
+let button = document.getElementById("myBtn")
+
+let span = document.getElementsByClassName("close")[0];
+
+button.onclick = function(){
+    modal.style.display = "block";
+}
+
+span.onclick = function(){
+    modal.style.display = "none";
+}
+
+window.onclick = function(event){
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
